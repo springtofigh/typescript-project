93391e820536f816a22822420c27b2785205bd1a
BT <bahartofigh@gmail.com>
2025-03-07 19:01:23 +0330

    add login and register modals, get datas for login and register

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
+import { JSX, useState } from 'react';
+// import { Link } from 'react-router-dom';
+import LoginModal from "./LoginModal";
+import RegisterModal from "./RegisterModal";
 
 export default function Navbar() {
-    return (
-    <nav className="w-full bg-purple-500 shadow">
+    const [showRegisterModal, setShowRegisterModal] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(false);
+
+    const NAV: JSX.Element = (
+        <nav className="w-full bg-purple-500 shadow">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
             <div>
                 <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -10,7 +17,47 @@ export default function Navbar() {
                 </a>
                 </div>
             </div>
+
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-white hover:text-indigo-200">
+                                Home
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                Blogs
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                Dashboard
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                Aboutme
+                            </li> 
+            </ul>
+
+            <div className="mt-3 space-y-2 md:inline-block">
+                                <a
+                                    href="#"
+                                    onClick={() => setShowLoginModal(true)}
+                                    className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
+                                >
+                                    Sign in
+                                </a>
+                                <a
+                                    href="#"
+                                    onClick={() => setShowRegisterModal(true)}
+                                    className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+                                >
+                                    Sign up
+                                </a>
+            </div>
         </div>
-    </nav>
+        </nav>
+    )
+
+    return (
+        <>
+        {NAV}
+        {showRegisterModal ? (<RegisterModal setShowRegisterModal={setShowRegisterModal} />) : null}
+        {showLoginModal ? (<LoginModal setShowLoginModal={setShowLoginModal} />) : null}
+        </>
     )
 }
---
+++ src/components/RegisterModal.tsx
@@ -0,0 +1,97 @@
+import { Dispatch, FC, SetStateAction, useState } from "react";
+
+interface IProps {
+    setShowRegisterModal: Dispatch<SetStateAction<Boolean>>
+}
+
+const RgisterModal : FC<IProps> = ({ setShowRegisterModal }) => {
+    const [ fullName, setFullName ] = useState<string>("");
+    const [ username, setuserName ] = useState<string>("");
+    const [ password, setPasword ] = useState<string>("");
+
+    const ResetForm = (): void => {
+        setFullName("");
+        setuserName("");
+        setPasword("");
+    }
+
+    // اگر داخل onSubmit‌ فرم بودیم میشد از event استفاده کرد
+    const RegisterHandler = () => {
+        console.log(fullName, username, password);
+        ResetForm();
+        setShowRegisterModal(false);
+    }
+
+
+    return (
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div
+                className="fixed inset-0 w-full h-full bg-black opacity-40"
+                onClick={() => setShowRegisterModal(false)}
+                >
+            </div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+                <div className="relative max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+                    <div className="mt-3 sm:flex">
+                        <div className="mt-2 text-center sm:ml-4 sm:text-left">
+                            <h4 className="text-lg font-medium text-gray-800">
+                                Signup To Account
+                            </h4>
+                            <div>
+                                <div className=" items-center pt-6 sm:pt-0">
+                                    <div className="p-3 mt-6 overflow-hidden bg-white shadow-md sm:rounded-lg">
+                                        <form>
+                                        <div className="mt-4">
+                                                <div className="flex flex-col items-start">
+                                                    <input type="text" name="fullname" placeholder="Fullname"
+                                                        value={fullName}
+                                                        onChange={(e) => setFullName(e.target.value)}
+                                                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mt-4">
+                                                <div className="flex flex-col items-start">
+                                                    <input type="text" name="username" placeholder="Username"
+                                                        value={username}
+                                                        onChange={(e) => setuserName(e.target.value)}
+                                                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mt-4">
+                                                <div className="flex flex-col items-start">
+                                                    <input type="password" name="password" placeholder="Password"
+                                                        value={password}
+                                                        onChange={(e) => setPasword(e.target.value)}
+                                                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div className="items-center gap-2 mt-3 sm:flex">
+                                            <button
+                                                className="w-full mt-2 p-2.5 flex-1 text-white bg-green-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
+                                                onClick={RegisterHandler}
+                                            >
+                                                Login
+                                            </button>
+                                            <button
+                                                className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+                                                
+                                            >
+                                                Cancel
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RgisterModal;
