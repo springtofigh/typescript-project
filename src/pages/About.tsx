329064b11c59516691e931dbce6e97747e940c85
BT <bahartofigh@gmail.com>
2025-09-09 16:09:15 +0330

    change jsx to tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+
+function About() {
+  return (
+    <div>
+        <h1 className="text-lg p-4 text-center">Hi, It's Spring</h1>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>
+        <h1 className="text-lg p-4 text-center">Home</h1>
+    </div>
+  )
+}
+
+export default Home;
