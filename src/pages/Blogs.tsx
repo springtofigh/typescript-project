50441fcfdb4cdc68fca25f5b0e3e8f62aa912968
BT <bahartofigh@gmail.com>
2025-09-11 21:54:26 +0330

    get fake blogs data and write two useEffects for token and data

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -4,6 +4,7 @@ import { IBlog } from '../types/blog.types';
 import { useApiGet } from '../functions/FetchAPI';
 import { useCookies } from 'react-cookie';
 import { COOKIES_NAME } from "../enums/public.enums";
+import { getBlogs as getFakeBlogs } from './../functions/FakeDatas';
 
 function Blogs() {
   const {data, getAPIData, status} = useApiGet();
@@ -12,15 +13,46 @@ function Blogs() {
   const [blogs, setBlogs] = useState<IBlog[]>([]);
 
   useEffect(() => {
-      getAPIData("/blog", {
-          headers: {
-            //میشه حتی یک enum برای bearer داشا
-              authorization: `Bearer ${token}`
-          }
-      })
-      setBlogs(data?.data?.blogs || [])
-  }, [status])
+  if (data) {
+    console.log("API DATA:", data);  // Is there any data?
+    setBlogs(data?.data?.blogs || []);
+  }
+}, [data]);
 
+
+  // useEffect(() => {
+  //     getAPIData("/blog", {
+  //         headers: {
+  //           //میشه حتی یک enum برای bearer داشا
+  //             authorization: `Bearer ${token}`
+  //         }
+  //     })
+  //     setBlogs(data?.data?.blogs || [])
+  // }, [status])
+
+      useEffect(() => {
+        if (token) {
+        getAPIData("/blog", {
+            headers: {      
+            authorization: `Bearer ${token}`
+            }
+        });
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if (status as any === "success" && data?.data?.blogs) {
+        setBlogs(data.data.blogs);
+        }
+    }, [status, data]);
+
+      useEffect(() => {
+    if (data?.data?.blogs?.length > 0) {
+      setBlogs(data.data.blogs)
+    } else {
+      setBlogs(getFakeBlogs())
+    }
+  }, [data])
   
   return (
       <>
