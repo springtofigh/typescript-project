882c48c9520ee00184b91de7480b1f73eb8da811
BT <bahartofigh@gmail.com>
2025-09-16 14:25:34 +0330

    add delete function

--- src/functions/FetchAPI.ts
+++ src/functions/FetchAPI.ts
@@ -73,3 +73,24 @@ export const useApiGet = (): TGetApiResponse => {
         loading,
     }
 }
+
+export const useApiDelete = (): [Function] => {
+    const [status, setStatus] = useState<number>(0);
+    const [data, setData] = useState<any>();
+    const [error, setError] = useState<any>();
+
+    const getAPIDelete = async (path: string, options: AxiosRequestConfig = {}) : Promise<void> => {
+        try {
+            const axiosResponse = await axios.delete(`${BackEndURL}${path}`, options)
+            setData(await axiosResponse.data);
+            setStatus(axiosResponse.status || data.status );
+        } catch (err: AxiosError | any) {
+            setStatus(err?.response?.data?.status || 500);
+            setError(err?.response?.data);
+            console.log(error)
+            return alert(status + " - " + error?.message || err?.message)
+        }
+    }
+
+    return [getAPIDelete];
+}
